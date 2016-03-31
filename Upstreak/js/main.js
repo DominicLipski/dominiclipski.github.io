c89c8d07ab9d63d2a79741255f6ed1ebd60b04f3
DominicLipski <b45lipsky@rambler.ru>
2016-03-31 11:13:53 +0300

    first commit

---
+++ Upstreak/js/main.js
@@ -0,0 +1,12 @@
+$(document).ready(function(){
+	$('.slider > ul').bxSlider({
+		pager: false,
+		slideWidth: 251,
+		slideMargin: 10,
+        minSlides: 3,
+        maxSlides: 3,
+        moveSlides: 1,
+        prevText: '',
+        nextText: ''
+	});
+});
