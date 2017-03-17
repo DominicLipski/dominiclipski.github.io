3f12bcd534e04c0ed6517ef6e2ea822b92d0d50c
DominicLipski <b45lipsky@rambler.ru>
2017-03-17 12:43:57 +0300

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -1,12 +1,38 @@
 $(document).ready(function(){
-	$('.slider > ul').bxSlider({
-		pager: false,
-		slideWidth: 251,
-		slideMargin: 10,
-        minSlides: 3,
-        maxSlides: 3,
-        moveSlides: 1,
-        prevText: '',
-        nextText: ''
-	});
-});
+	$('.main-slider').slick({
+    dots: true,
+  	slidesToShow: 1,
+  	slidesToScroll: 1,
+  	autoplaySpeed: 2000,
+    prevArrow:'<span class="main-slider__arrow main-slider__arrow-left"></span>',
+    nextArrow:'<span class="main-slider__arrow main-slider__arrow-right"></span>',
+    dotsClass: 'main-slider__pagination',
+  });
+  $('.products-slider').slick({
+    slidesToShow: 4,
+    slidesToScroll: 1,
+    autoplaySpeed: 2000,
+    prevArrow:'<span class="main-slider__arrow main-slider__arrow-left"></span>',
+    nextArrow:'<span class="main-slider__arrow main-slider__arrow-right"></span>',
+    responsive: [
+      {
+        breakpoint: 1199,
+        settings: {
+          slidesToShow: 3,
+        }
+      },
+      {
+        breakpoint: 991,
+        settings: {
+          slidesToShow: 2,
+        }
+      },
+      {
+        breakpoint: 767,
+        settings: {
+          slidesToShow: 1,
+        }
+      }
+    ]
+  });
+})
